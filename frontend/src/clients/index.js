841053a790a06ce82d000517a7b9459f046784d4
Shakti242 <shaktipatel2421@gmail.com>
2025-02-05 10:26:44 -0500

    Remove sensitive info and fix history

--- frontend/src/clients/index.js
+++ frontend/src/clients/index.js
@@ -1,16 +1,16 @@
 import AWS from 'aws-sdk';
 import { REGION } from '../constants';
 
-const ACCESS_KEY_ID = 'AKIAUZPNLVLURUPK2SOB';
-const SECRET_ACCESS_KEY = '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';
+const ACCESS_KEY_ID = '';
+const SECRET_ACCESS_KEY =
 
-AWS.config.update({
-  region: REGION,
-  credentials: new AWS.Credentials({
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY
-  })
-});
+  AWS.config.update({
+    region: REGION,
+    credentials: new AWS.Credentials({
+      accessKeyId: ACCESS_KEY_ID,
+      secretAccessKey: SECRET_ACCESS_KEY
+    })
+  });
 
 export const S3 = new AWS.S3();
 
